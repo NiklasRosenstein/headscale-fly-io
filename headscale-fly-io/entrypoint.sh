fe82c0048407cee9382468656c0043602bfc2c73
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-10-14 23:50:52 +0200

    Factor our Litestream entrypoint

--- headscale-fly-io/entrypoint.sh
+++ headscale-fly-io/entrypoint.sh
@@ -50,7 +50,6 @@ on_error() {
 trap 'on_error' EXIT
 
 HEADSCALE_CONFIG_PATH=/etc/headscale/config.yaml
-HEADSCALE_DB_PATH=/var/lib/headscale/db.sqlite
 NOISE_PRIVATE_KEY_FILE=/var/lib/headscale/noise_private.key
 
 # This file must be configured through a secret and mounted via the fly.toml configuration.
@@ -66,44 +65,6 @@ assert_is_set AWS_REGION
 assert_is_set AWS_ENDPOINT_URL_S3
 assert_is_set BUCKET_NAME
 assert_is_set AGE_SECRET_KEY
-
-export LITESTREAM_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
-export LITESTREAM_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-AGE_PUBLIC_KEY="$(echo "$AGE_SECRET_KEY" | age-keygen -y)"
-
-info "generating /etc/litestream.yml"
-cat <<EOF >/etc/litestream.yml
-dbs:
-- path: /var/lib/headscale/db.sqlite
-  replicas:
-  # See https://litestream.io/reference/config/#s3-replica
-  - type: s3
-    bucket: $BUCKET_NAME
-    path: headscale.db
-    region: $AWS_REGION
-    endpoint: $AWS_ENDPOINT_URL_S3
-    # See https://litestream.io/reference/config/#replica-settings
-    sync-interval: "${LITESTREAM_SYNC_INTERVAL:-10s}"
-    # See https://litestream.io/reference/config/#encryption
-    age:
-      identities:
-      - "$AGE_SECRET_KEY"
-      recipients:
-      - "$AGE_PUBLIC_KEY"
-    # See https://litestream.io/reference/config/#retention-period
-    retention: "${LITESTREAM_RETENTION:-24h}"
-    retention-check-interval: "${LITESTREAM_RETENTION_CHECK_INTERVAL:-1h}"
-    # https://litestream.io/reference/config/#validation-interval
-    validation-interval: "${LITESTREAM_VALIDATION_INTERVAL:-12h}"
-EOF
-
-info "configuring mc"
-mc alias set s3 "$AWS_ENDPOINT_URL_S3" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
-
-if [ "${ENTRYPOINT_DEBUG:-}" = "true" ]; then
-    debug "ENTRYPOINT_DEBUG is set: set -x"
-    set -x
-fi
 
 # The HEADSCALE_SERVER_URL variable was removed.
 if [ -n "${HEADSCALE_SERVER_URL:-}" ]; then
@@ -145,19 +106,7 @@ if [ "${ENTRYPOINT_DEBUG:-}" = "true" ]; then
     debug "end contents of $HEADSCALE_CONFIG_PATH"
 fi
 
-# Check if there is an existing database to import from S3.
-if [ "${IMPORT_DATABASE:-}" = "true" ] && mc find "s3/$BUCKET_NAME/import-db.sqlite" 2> /dev/null > /dev/null; then
-    info "found \"import-db.sqlite\" in bucket, importing that database instead of restoring with litestream"
-    mc cp "s3/$BUCKET_NAME/import-db.sqlite" "$HEADSCALE_DB_PATH"
-elif [ "${LITESTREAM_ENABLED:-true}" = "true" ]; then
-    info_run litestream restore -if-db-not-exists -if-replica-exists -replica s3 "$HEADSCALE_DB_PATH"
-fi
-
 maybe_idle
 
-# Run Headscale.
-if [ "${LITESTREAM_ENABLED:-true}" = "true" ]; then
-    info_run exec litestream replicate -exec "headscale serve"
-else
-    info_run exec headscale serve
-fi
+export LITESTREAM_DATABASE_PATH=/var/lib/headscale/db.sqlite
+exec /etc/headscale/litestream-entrypoint.sh "headscale serve"
